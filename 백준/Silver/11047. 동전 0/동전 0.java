5ca1786f1eef7766e15cdb1d1b02b1afd498a5ed
yuzzin <77273340+babo0121@users.noreply.github.com>
2023-01-25 16:55:34 +0900

    [Silver IV] Title: 동전 0, Time: 128 ms, Memory: 14264 KB -BaekjoonHub

---
+++ 백준/Silver/11047. 동전 0/동전 0.java
@@ -0,0 +1,35 @@
+
+// 그리디 알고리즘 - 동전 0
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.sql.SQLOutput;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine(), " ");
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        int[] coin = new int[N];
+
+        for(int i = 0; i < N; i++) {
+            coin[i] = Integer.parseInt(br.readLine());
+        }
+
+        int count = 0;
+
+        for(int i = N - 1; i >= 0; i--){
+            if(coin[i] <= K){
+                count += (K / coin[i]);
+                K = K % coin[i];
+            }
+        }
+        System.out.println(count);
+    }
+}
