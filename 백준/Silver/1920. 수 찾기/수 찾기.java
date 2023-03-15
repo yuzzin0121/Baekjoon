6038aaf9c2a38c1494e6182ca28f58fbb9f02d87
yuzzin <77273340+babo0121@users.noreply.github.com>
2023-03-15 21:52:50 +0900

    [Silver IV] Title: 수 찾기, Time: 600 ms, Memory: 44996 KB -BaekjoonHub

---
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -0,0 +1,41 @@
+// 정렬 - 수 찾기
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        for(int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        int M = Integer.parseInt(br.readLine());
+
+        st = new StringTokenizer(br.readLine(), " ");
+
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < M; i++) {
+
+            // arr에 값이 있을 경우 1, 없을 경우 0을 출력
+            if(Arrays.binarySearch(arr, Integer.parseInt(st.nextToken())) >= 0) {
+                sb.append(1).append('\n');
+            }
+            else {
+                sb.append(0).append('\n');
+            }
+        }
+        System.out.println(sb);
+    }
+}
