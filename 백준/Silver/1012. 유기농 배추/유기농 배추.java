18a32c86bc5ed8f6aec34a7e4f1c8df6a4b993de
yuzzin <77273340+babo0121@users.noreply.github.com>
2023-04-06 00:34:55 +0900

    [Silver II] Title: 유기농 배추, Time: 160 ms, Memory: 15732 KB -BaekjoonHub

---
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.java
@@ -0,0 +1,67 @@
+
+// 깊이우선탐색 - 유기농 배추
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int M, N, K;
+    static int[][] cabbage;
+    static boolean[][] visit;
+    static int count;
+    static int[] dx = { 0, -1, 0, 1 };
+    static int[] dy = { 1, 0, -1, 0 };
+
+    public static void main(String[] args) throws NumberFormatException, IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < T; i++) {
+            count = 0;
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+            M = Integer.parseInt(st.nextToken());
+            N = Integer.parseInt(st.nextToken());
+            cabbage = new int[M][N];
+            visit = new boolean[M][N];
+
+            // 배추가 심어져 있는 위치의 개수
+            K = Integer.parseInt(st.nextToken());
+            for (int j = 0; j < K; j++) {
+                st = new StringTokenizer(br.readLine(), " ");
+                int p1 = Integer.parseInt(st.nextToken());
+                int p2 = Integer.parseInt(st.nextToken());
+                cabbage[p1][p2] = 1; // 배추의 위치에 1 넣어주기
+            }
+
+            for (int x = 0; x < M; x++) {
+                for (int y = 0; y < N; y++) {
+                    if (cabbage[x][y] == 1 && !visit[x][y]) {
+                        dfs(x, y);
+                        count++;
+                    }
+                }
+            }
+
+            System.out.println(count);
+        }
+    }
+    static void dfs(int x, int y) {
+        visit[x][y] = true;
+
+        for (int i = 0; i < 4; i++) {
+            int cx = x + dx[i];
+            int cy = y + dy[i];
+
+            if (cx >= 0 && cy >= 0 && cx < M && cy < N) {
+                if (!visit[cx][cy] && cabbage[cx][cy] == 1) {
+                    dfs(cx, cy);
+                }
+            }
+
+        }
+
+    }
+}
